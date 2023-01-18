82fadd811a36f5e26d4fe2cac037cb401921403e
Michell2022 <sistemas.angel04@gmail.com>
2023-01-18 10:07:27 -0500

    Estilo header y login + rutas

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { BreakingComponent } from './breaking/breaking.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    BreakingComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HomeComponent,
+    LoginComponent
   ]
 })
 export class PagesModule { }
